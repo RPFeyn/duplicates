5c63dd602006a3618a18960b65db46c25c63bf19
Trey Sellers <sellers3.trey@gmail.com>
2015-03-29 12:55:16 -0400

    Refactored main algo into find_duplicates() and private functions.  Improved exception handling and error messages. Refactored _purge() to shrink search maps.  Improved docstrings.

--- duplicates.py
+++ duplicates.py
@@ -3,21 +3,27 @@ import os, hashlib, sys
 import argparse
 from collections import defaultdict
 
-#TODO: Refactor main algorithm (building of maps, filtering sizemap, etc) so functions are more single purpose
+
 def main():
     parser = _make_parser()
     args = parser.parse_args()
-    output = _check_output(args.output_filename)
-    h=md5hash_dict(args.input_dirs, args.verbose)
+    output = _check_output(args.output_filename) #Want to run this before find_duplicates because 
+                                                 #user can cancel if s/he would overwrite something important
+    h=find_duplicates(args.input_dirs, args.verbose)
     output_duplicates(h, output, args.verbose)
-    #output.close()
+    #output.close() #Not strictly necessary
    
 
-def md5hash_dict(base_paths, verbose=False) :
-    '''Builds a dict mapping the md5 hash of files in path (recursively searched) to filenames of duplicates'''
-    if not _check_paths(base_paths) :
-        exit(1)
-    sizemap = _build_size_dict(base_paths, verbose)
+def find_duplicates(base_paths, verbose=False) :
+    '''Searches over dirs in base_paths (and their subdirs) to group files with equivalent contents 
+    into a dict {search criterion(md5hash) : [filelist] } to filenames'''
+    sizemap=_build_size_dict(base_paths, verbose)
+    return _md5hash_dict(sizemap, verbose)
+
+
+def _md5hash_dict(sizemap, verbose=False) :
+    '''Builds a dict mapping the md5 hash of files in path (recursively searched) to filenames of duplicates.
+    sizemap is assumed to be a dict mapping { initial_criterion : [list of duplicate candidates] }'''
     if verbose :
         print("Building md5 hashes of files (this may take a while)")
     hashmap = defaultdict(list)
@@ -29,20 +35,25 @@ def md5hash_dict(base_paths, verbose=False) :
                 h=hashlib.md5(d).hexdigest()
                 hashmap[h].append(fullname)
             except OSError:
-                print("Can't open {}".format(os.path.abspath(fullname)))
-                continue
+                print("Warning: Can't open {}".format(os.path.abspath(fullname)), file=sys.stderr)
+                continue #Just skip files we can't open
     return hashmap
 
 
 def _build_size_dict(base_paths, verbose=False) :
-    '''Builds a dictionary mapping {file size in bytes : list of files with that filesize}, ignoring size 0 files, and only returning those that have duplicates'''
-    #NOTE: Assumes paths have already been checked by _check_paths.  In normal operation this has been done in md5hash_dict
-    #TODO: Change above behavior? Probably need to refactor
+    '''Builds a dictionary mapping {file size in bytes : list of files with that filesize},
+    ignoring size 0 files, and only returning those that have duplicates.''' 
+    try:
+        _check_paths(base_paths)
+    except OSError:
+        print("Error: Bad input directory or directories. Cannot continue.", file=sys.stderr)
+        exit(1)
     sizemap=defaultdict(list)
     if verbose :
         print("Creating candidates for duplicates based on filesize")
+    #Maybe: Write this as generator/comprehension? Would be less readable nested this deep
     for base_path in base_paths :
-        for (dirpath, dirnames, filenames) in os.walk(base_path) :
+        for (dirpath, _ , filenames) in os.walk(base_path) :
             for filename in filenames :
                 fullname = os.path.join(dirpath, filename)
                 try:
@@ -52,18 +63,25 @@ def _build_size_dict(base_paths, verbose=False) :
                     continue
                 if sz > 0 :
                     sizemap[sz].append(fullname)
-    to_delete=set()
-    for (n_bytes, filelist) in sizemap.items() :
-        if len(filelist) <= 1 :
-            to_delete.add(n_bytes)
-    for n in to_delete:
-        del sizemap[n]
+    sizemap=_purge(sizemap) #Consider inlining _purge so sizemap doesn't have to get passed around
     if verbose :
         print("Found {} unique filesizes that are duplicate candidates.".format(len(sizemap)))
     return sizemap
 
 
+def _purge(search_map) :
+    '''Takes a dict {search criterion : [filelist]} and removes (k,v) pairs with len(v) <= 1,
+    i.e. those without duplicates'''
+    to_delete=set() #Can't operate on the dict as we iterate over it
+    for (s, filelist) in search_map.items() :
+        if len(filelist) <= 1 :
+            to_delete.add(s)
+    for n in to_delete :
+        del search_map[n]
+    return search_map
+
 def _make_parser() :
+    '''Command line parser'''
     parser = argparse.ArgumentParser(description="Searches directory and its subdirectories for files with duplicate contents, optionally writing out duplicates to either a text file or stdout")
     parser.add_argument('-o', '--output', help='optional output text file', nargs='?',  default=sys.stdout, dest="output_filename")
     parser.add_argument("input_dirs", nargs='+', help='Input directories to be search, separated by whitespace.  Note that subdirectories are searched')
@@ -73,6 +91,7 @@ def _make_parser() :
 
 
 def output_duplicates(hashmap, output=sys.stdout, verbose=False) :
+    '''Simple printer function that takes a dict {search_criteria : [files]}, and prints the files which have duplicates.  If verbose, will print duplicates to both stdout and the output file, if they're different.'''
     count = 0
     for (k, v) in hashmap.items() :
         if len(v) > 1 :
@@ -86,15 +105,19 @@ def output_duplicates(hashmap, output=sys.stdout, verbose=False) :
 
 
 def _check_paths(paths) :
+    '''Input sanitization on input paths: checks all paths, and throws an exception if any fail'''
     paths_ok=True
     for path in paths :
         if not os.path.exists(path) :
-            print("Error: {} is not an existing path".format(base_path))
+            print("Error: {} is not an existing path".format(path), file=sys.stderr)
             paths_ok=False
+    if not paths_ok :
+        raise OSError("Couldn't open some input directories")
     return paths_ok
 
 
 def _check_output(out) :
+    '''Takes out to either be sys.stdout, or a filename of a text file to write to.  Ensures user doesn't accidentally overwrite something they want to keep.  Returns an appropriate output file handle if possible'''
     #TODO: This is a mess
     if out is sys.stdout :
         return out
@@ -108,10 +131,10 @@ def _check_output(out) :
     try:
         f=open(out, 'w')
     except PermissionError :
-        print("Permission error, couldn't write to {}.  Check that you have write permissions.".format(out))
+        print("Error: Couldn't write to {}. Check that you have write permissions.".format(out), file=sys.stderr)
         exit(1)
     except :
-        print("Unknown error in opening {}".format(out))
+        print("Error: Unknown error in opening {}".format(out), file=sys.stderr)
         exit(1)
     return f
 
