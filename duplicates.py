3057be7701b458a510bf55505ff5097d11037468
Trey Sellers <sellers3.trey@gmail.com>
2015-03-29 01:03:59 -0400

    Converted to argparse, added support for multiple input directories

--- duplicates.py
+++ duplicates.py
@@ -1,65 +1,85 @@
 #!/usr/bin/env python3
 import os, hashlib, sys
+import argparse
 
-def md5hash_dict(base_path):
+
+def main():
+    parser = _make_parser()
+    args = parser.parse_args()
+    output = _check_output(args.output_filename)
+    h=md5hash_dict(args.input_dirs, args.verbose)
+    output_duplicates(h, output, args.verbose)
+    output.close()
+   
+def md5hash_dict(base_paths, verbose=False) :
     '''Builds a dict mapping the md5 hash of files in path (recursively searched) to filenames of duplicates'''
-    if not os.path.exists(base_path) :
-        print("Error: {} is not an exising path".format(base_path))
+    if not _check_paths(base_paths) :
         exit(1)
+    if verbose :
+        print("Building md5 hashes of files (this may take a while)")
     hashmap = {}
-    print("Building md5 hashes of files (this may take a while)")
-    for (dirpath, dirnames, filenames) in os.walk(base_path) :
-        print("On directory ", dirpath)
-        for filename in filenames :
-            fullname = os.path.join(dirpath, filename)
-            try:
-                with open(fullname, 'rb') as f :
-                    d = f.read()
-                h = hashlib.md5(d).hexdigest()
-                filelist = hashmap.setdefault(h, [])
-                filelist.append(fullname)
-            except OSError:
-                print("Can't open {}".format(os.path.abspath(fullname)))
-                continue
+    for base_path in base_paths :
+        for (dirpath, dirnames, filenames) in os.walk(base_path) :
+            if verbose :
+                print("On directory ", dirpath)
+            for filename in filenames :
+                fullname = os.path.join(dirpath, filename)
+                try:
+                    with open(fullname, 'rb') as f :
+                        d = f.read()
+                    h = hashlib.md5(d).hexdigest()
+                    filelist = hashmap.setdefault(h, [])
+                    filelist.append(fullname)
+                except OSError:
+                    #TODO: if verbose on this line or not?  Probably better to leave it...
+                    print("Can't open {}".format(os.path.abspath(fullname)))
+                    continue
     return hashmap
 
-def output_duplicates(hashmap, output=None) :
-    if not output or output is sys.stdout:
-        output = sys.stdout
-        _printer(hashmap, output)
-    else:
-        with open(output, "w") as f:
-            _printer(hashmap, f)
 
-def _printer(hashmap, out) :
+def _make_parser() :
+    parser = argparse.ArgumentParser(description="Searches directory and its subdirectories for files with duplicate contents, optionally writing out duplicates to either a text file or stdout")
+    parser.add_argument('-o', '--output', help='optional output text file', nargs='?',  default=sys.stdout, dest="output_filename")
+    parser.add_argument("input_dirs", nargs='+', help='Input directories to be search, separated by whitespace.  Note that subdirectories are searched')
+    parser.add_argument('-v', '--verbose', help='Increase verbosity', action='store_true', default=False)
+    return parser
+
+
+
+def output_duplicates(hashmap, output=sys.stdout, verbose=False) :
+    count = 0
     for (k, v) in hashmap.items() :
         if len(v) > 1 :
-            print('Duplicates: ', v, file=out)
+            count += 1
+            print('Duplicates: ', v, file=output)
+            if not output is sys.stdout and verbose :
+                print('Duplicates: ', v, file=sys.stdout)
+    print("\nFound {} unique files with at least 1 duplicate".format(count))
 
-def _print_usage(args) :
-    (_, script_name) = os.path.split(args[0])
-    print("\nUsage: {} search_directory [output_filename]\n".format(script_name))
-    print(script_name, " searches search_directory and all subdirectories for files with duplicate contents.")
-    print("Defaults to printing to stdout if no output file is given.")
 
-if __name__ == "__main__" :
-    if not (len(sys.argv) == 2 or len(sys.argv) == 3) :
-        _print_usage(sys.argv)
-        exit(1)
 
-    if len(sys.argv) == 3 and os.path.isfile(sys.argv[2]) :
-        answer = input("File {} exists and will be overwritten: continue? (y/N)".format(sys.argv[2]))
+def _check_paths(paths) :
+    paths_ok=True
+    for path in paths :
+        if not os.path.exists(path) :
+            print("Error: {} is not an existing path".format(base_path))
+            paths_ok=False
+    return paths_ok
+
+
+def _check_output(out) :
+    if out is sys.stdout :
+        return out
+    if os.path.exists(out) :
+        answer = input("Output file {} exists and will be overwritten: continue? (y/N)".format(out))
         while answer not in "yYnN" :
             answer = input("Please enter y/n:")
         if answer in "nN" :
-            print("Got N, exiting")
-            exit(0)
+            print("Got {}, exiting".format(answer))
+            exit(1)
+    f=open(out, 'w')
+    return f
 
-    h=md5hash_dict(sys.argv[1])
-    if len(sys.argv) == 3:
-        output_duplicates(h, sys.argv[2])
-        print("Done.  Duplicates located in {}".format(sys.argv[2]))
-    else :
-        print("No output file given.  Printing duplicates to stdout.")
-        output_duplicates(h, sys.stdout)
-        print("Done.")
+
+if __name__ == "__main__" :
+    main()
