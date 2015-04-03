e4e7db09fe764b249e4bfb424f7d8acb5821a050
Trey Sellers <sellers3.trey@gmail.com>
2015-04-02 22:49:09 -0400

    Changed function names. Added option to not follow symlinks, updated comments

--- duplicates.py
+++ duplicates.py
@@ -3,31 +3,32 @@ import os, hashlib, sys, argparse
 from collections import defaultdict
 '''Searches and finds a list of files which are duplicates in content (but not necessarily filename) in user specified directories.'''
 
-'''Options I'd like to add:
-   - Finer control over descending into subdirs, symlinks
+'''Todo list
+   - With -L (no symlinks), size keeps trying to get the size of files it can't get to
+   - Finer control over descending into subdirs?
    - Printing options: compact printing (1 set of duplicates per line, useful for sort) vs pretty printing (one file per line, for example)
    - Figure out if we can increase performance -- still slowish on big dirs
    - Some indication if entire dirs are duplicated?
    - Maybe abstract function for building dicts to allow easier extension/changes
-   - _check_output() is a mess -- I don't like that I have different ways of handling sys.stdout vs filenames'''
+   - check_output() is a mess -- I don't like that I have different ways of handling sys.stdout vs filenames'''
 
 
 def main():
-    parser = _make_parser()
+    parser = make_parser()
     args = parser.parse_args()
-    output = _check_output(args.output_filename)     #Want to call _check_output() before find_duplicates() because 
-    h=find_duplicates(args.input_dirs, args.verbose) #user can cancel execution in _check_output() 
+    output = check_output(args.output_filename)     #Want to call check_output() before find_duplicates() because 
+    h=find_duplicates(args.input_dirs, args.verbose, args.follow_links) #user can cancel execution in check_output() 
     output_duplicates(h, output, args.verbose)
    
 
-def find_duplicates(base_paths, verbose=False) :
+def find_duplicates(base_paths, verbose=False, follow_links=True) :
     '''Searches over dirs in base_paths (and their subdirs) to group files with equivalent contents 
     into a dict {search criterion(md5hash) : [filelist] } to filenames'''
-    sizemap=_build_size_dict(base_paths, verbose)
-    return _md5hash_dict(sizemap, verbose)
+    sizemap=build_size_dict(base_paths, verbose, follow_links)
+    return md5hash_dict(sizemap, verbose)
 
 
-def _md5hash_dict(sizemap, verbose=False) :
+def md5hash_dict(sizemap, verbose=False) :
     '''Builds a dict mapping the md5 hash of files in path (recursively searched) to filenames of duplicates.
     sizemap is assumed to be a dict mapping { initial_criterion : [list of duplicate candidates] }'''
     if verbose :
@@ -43,15 +44,15 @@ def _md5hash_dict(sizemap, verbose=False) :
             except OSError:
                 print("Warning: Can't open {}".format(os.path.abspath(fullname)), file=sys.stderr)
                 continue 
-    hashmap=_purge(hashmap)
+    hashmap=purge(hashmap)
     return hashmap
 
 
-def _build_size_dict(base_paths, verbose=False) :
+def build_size_dict(base_paths, verbose=False, follow_links=True) :
     '''Builds a dictionary mapping {file size in bytes : list of files with that filesize},
     ignoring size 0 files, and only returning those that have duplicates.''' 
     try:
-        _check_paths(base_paths)
+        check_input_paths(base_paths)
     except OSError:
         print("Error: Bad input directory or directories. Cannot continue.", file=sys.stderr)
         sys.exit(1)
@@ -60,7 +61,7 @@ def _build_size_dict(base_paths, verbose=False) :
         print("Creating candidates for duplicates based on filesize")
     #Maybe: Write this as generator/comprehension? Would be less readable nested this deep
     for base_path in base_paths :
-        for (dirpath, _ , filenames) in os.walk(base_path) :
+        for (dirpath, _ , filenames) in os.walk(base_path, followlinks=follow_links) :
             for filename in filenames :
                 fullname = os.path.join(dirpath, filename)
                 try:
@@ -70,13 +71,13 @@ def _build_size_dict(base_paths, verbose=False) :
                     continue
                 if sz > 0 :
                     sizemap[sz].append(fullname)
-    sizemap=_purge(sizemap) #Consider inlining _purge so sizemap doesn't have to get passed around: For now, _purge() only used here
+    sizemap=purge(sizemap) #Consider inlining purge so sizemap doesn't have to get passed around: For now, purge() only used here
     if verbose :
         print("Found {} unique filesizes that are duplicate candidates.".format(len(sizemap)))
     return sizemap
 
 
-def _purge(search_map) :
+def purge(search_map) :
     '''Takes a dict {search criterion : [filelist]} and removes (k,v) pairs with len(v) <= 1,
     i.e. those without duplicates. Sorts individual filelists for easier reading'''
     to_delete=set() #Can't operate on the dict as we iterate over it
@@ -90,26 +91,28 @@ def _purge(search_map) :
     return search_map
 
 
-def _make_parser() :
+def make_parser() :
     '''Command line parser'''
     parser = argparse.ArgumentParser(description="Searches directory and its subdirectories for files with duplicate contents, optionally writing out duplicates to either a text file or stdout")
+    
     parser.add_argument('-o', '--output', help='optional output text file', nargs='?',  default=sys.stdout, dest="output_filename")
     parser.add_argument("input_dirs", nargs='+', help='Input directories to be search, separated by whitespace.  Note that subdirectories are searched')
     parser.add_argument('-v', '--verbose', help='Increase verbosity', action='store_true', default=False)
+    parser.add_argument('-L', '--no-follow-links', dest='follow_links', help="Don't follow symlinks", action='store_false', default=True) #if -L is present, pass followlinks=false to os.walk
     return parser
 
 
 def output_duplicates(hashmap, output=sys.stdout, verbose=False) :
     '''Simple printer function that takes a dict {search_criteria : [files]}, and prints the files which have duplicates.  If verbose, will print duplicates to both stdout and the output file, if they're different.'''
-    _printer(hashmap, output)
+    printer(hashmap, output)
     if not output is sys.stdout and verbose :
-        _printer(hashmap, sys.stdout)
+        printer(hashmap, sys.stdout)
     print("\nFound {} unique files with at least 1 duplicate.".format(len(hashmap)))
     if not output is sys.stdout :
         print("It may be helpful to run 'sort {} > some_new_output.txt' ".format(output.name))
 
 
-def _printer(d, out) :
+def printer(d, out) :
     for (k, v) in d.items() :
         print('[ ', file=out, end=" ")
         for f in v : 
@@ -117,7 +120,7 @@ def _printer(d, out) :
         print(']', file=out)
 
 
-def _check_paths(paths) :
+def check_input_paths(paths) :
     '''Input sanitization on input paths: checks all paths, and throws an exception if any fail'''
     paths_ok=True
     for path in paths :
@@ -128,7 +131,7 @@ def _check_paths(paths) :
         raise OSError("Couldn't open some input directories")
     return paths_ok
 
-def _check_for_overwrite(out) :
+def ask_on_overwrite(out) :
     '''Checks if filename out exists, queries user how to proceed'''
     if os.path.exists(out) :
         answer = input("Output file {} exists and will be overwritten: continue? (y/N)".format(out))
@@ -139,11 +142,11 @@ def _check_for_overwrite(out) :
                 sys.exit(0)
 
 
-def _check_output(out) :
+def check_output(out) :
     '''Takes out to either be sys.stdout, or a filename of a text file to write to.  Ensures user doesn't accidentally overwrite something they want to keep.  Returns an appropriate output file handle if possible'''
     if out is sys.stdout :
         return out
-    _check_for_overwrite(out)
+    ask_on_overwrite(out)
     try:
         f=open(out, 'w')
     except PermissionError :
