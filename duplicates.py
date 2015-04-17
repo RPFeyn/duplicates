cfd15158d138e511f0d5f67311fcd079332a2078
Trey Sellers <sellers3.trey@gmail.com>
2015-04-17 01:34:32 -0400

    refactored and renamed purge into purge_uniques and sort_filenames

--- duplicates.py
+++ duplicates.py
@@ -4,20 +4,21 @@ from collections import defaultdict
 '''Searches and finds a list of files which are duplicates in content (but not necessarily filename) in user specified directories.'''
 
 '''Todo list
-   - With -L (no symlinks), size keeps trying to get the size of files it can't get to
+   - Maybe abstract function for building dicts to allow easier extension/changes
+   - With -L (no symlinks), size portion keeps trying to get the size of files it can't get to
    - Finer control over descending into subdirs?
    - Printing options: compact printing (1 set of duplicates per line, useful for sort) vs pretty printing (one file per line, for example)
    - Figure out if we can increase performance -- still slowish on big dirs
    - Some indication if entire dirs are duplicated?
-   - Maybe abstract function for building dicts to allow easier extension/changes
-   - check_output() is a mess -- I don't like that I have different ways of handling sys.stdout vs filenames'''
+   - get_output_file() is a mess -- I don't like that I have different ways of handling sys.stdout vs filenames'''
 
 
 def main():
     parser = make_parser()
     args = parser.parse_args()
-    output = check_output(args.output_filename)     #Want to call check_output() before find_duplicates() because 
-    h=find_duplicates(args.input_dirs, args.verbose, args.follow_links) #user can cancel execution in check_output() 
+    #Call get_output_file() before find_duplicates() -- user can cancel in the former
+    output = get_output_file(args.output_filename)     
+    h=find_duplicates(args.input_dirs, args.verbose, args.follow_links) 
     output_duplicates(h, output, args.verbose)
    
 
@@ -29,12 +30,13 @@ def find_duplicates(base_paths, verbose=False, follow_links=True) :
 
 
 def md5hash_dict(sizemap, verbose=False) :
-    '''Builds a dict mapping the md5 hash of files in path (recursively searched) to filenames of duplicates.
-    sizemap is assumed to be a dict mapping { initial_criterion : [list of duplicate candidates] }'''
+    '''Builds a dict mapping the md5 hash of files in path (recursively searched) 
+    to filenames of duplicates.  sizemap is assumed to be a dict mapping 
+    { initial_criterion : [list of duplicate candidates] }'''
     if verbose :
         print("Building md5 hashes of files (this may take a while)")
     hashmap = defaultdict(list)
-    for (_, filenames) in sizemap.items() : #Don't care about actual sizes, only which files have == size
+    for (_, filenames) in sizemap.items() : 
         for fullname in filenames :
             try :
                 with open(fullname, 'rb') as f:
@@ -44,7 +46,7 @@ def md5hash_dict(sizemap, verbose=False) :
             except OSError:
                 print("Warning: Can't open {}".format(os.path.abspath(fullname)), file=sys.stderr)
                 continue 
-    hashmap=purge(hashmap)
+    hashmap=sort_filenames(purge_uniques(hashmap))
     return hashmap
 
 
@@ -71,25 +73,28 @@ def build_size_dict(base_paths, verbose=False, follow_links=True) :
                     continue
                 if sz > 0 :
                     sizemap[sz].append(fullname)
-    sizemap=purge(sizemap) #Consider inlining purge so sizemap doesn't have to get passed around: For now, purge() only used here
+    sizemap=purge_uniques(sizemap) #Consider inlining purge_uniques so sizemap doesn't have to get passed around: For now, purge_uniques() only used here
     if verbose :
         print("Found {} unique filesizes that are duplicate candidates.".format(len(sizemap)))
     return sizemap
 
 
-def purge(search_map) :
-    '''Takes a dict {search criterion : [filelist]} and removes (k,v) pairs with len(v) <= 1,
-    i.e. those without duplicates. Sorts individual filelists for easier reading'''
+def purge_uniques(search_map) :
+    '''Takes a dict {search criterion : [filelist]} and removes (k,v) pairs with
+    len(v) <= 1, i.e. those without duplicates.'''
     to_delete=set() #Can't operate on the dict as we iterate over it
     for (s, filelist) in search_map.items() :
         if len(filelist) <= 1 :
             to_delete.add(s)
-        else :
-            filelist.sort()
     for n in to_delete :
         del search_map[n]
     return search_map
 
+
+def sort_filenames(search_map) :
+    for (_, filelist) in search_map.items() :
+        filelist.sort()
+    return search_map
 
 def make_parser() :
     '''Command line parser'''
@@ -142,7 +147,7 @@ def ask_on_overwrite(out) :
                 sys.exit(0)
 
 
-def check_output(out) :
+def get_output_file(out) :
     '''Takes out to either be sys.stdout, or a filename of a text file to write to.  Ensures user doesn't accidentally overwrite something they want to keep.  Returns an appropriate output file handle if possible'''
     if out is sys.stdout :
         return out
